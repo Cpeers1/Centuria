ebb05fbca8cae059bde3798d8d9fbcb5f70224cd
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2022-05-29 05:25:57 +0200

    Prepare for a16

--- src/main/java/org/asf/emuferal/EmuFeral.java
+++ src/main/java/org/asf/emuferal/EmuFeral.java
@@ -47,7 +47,7 @@ import org.asf.rats.ConnectiveServerFactory;
 
 public class EmuFeral {
 	// Update
-	public static final String SERVER_UPDATE_VERSION = "1.0.0.A15";
+	public static final String SERVER_UPDATE_VERSION = "1.0.0.A16";
 	public static final String DOWNLOAD_BASE_URL = "https://aerialworks.ddns.net/extra/emuferal";
 
 	// Configuration
@@ -283,7 +283,7 @@ public class EmuFeral {
 		System.out.println("                              EmuFeral                              ");
 		System.out.println("                       Fer.al Server Emulator                       ");
 		System.out.println("                                                                    ");
-		System.out.println("                         Version: 1.0.0.A15                         "); // not doing this
+		System.out.println("                         Version: 1.0.0.A16                         "); // not doing this
 																									// dynamically as
 																									// centering is a
 																									// pain
