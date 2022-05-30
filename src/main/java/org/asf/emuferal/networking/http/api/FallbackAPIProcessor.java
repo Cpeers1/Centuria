32c0b44b3efd2ebc200e60e48a91e7e55e78c095
Cpeers1 <Owen9456@gmail.com>
2022-05-30 14:14:24 -0600

    WIP changes for social.
    Signed-off-by: Cpeers1 <Owen9456@gmail.com>

--- src/main/java/org/asf/emuferal/networking/http/api/FallbackAPIProcessor.java
+++ src/main/java/org/asf/emuferal/networking/http/api/FallbackAPIProcessor.java
@@ -1,6 +1,7 @@
 package org.asf.emuferal.networking.http.api;
 
 import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.Socket;
 import java.text.SimpleDateFormat;
 import java.util.Base64;
@@ -19,6 +20,7 @@ import org.asf.rats.processors.HttpUploadProcessor;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 
 public class FallbackAPIProcessor extends HttpUploadProcessor {
 
@@ -94,20 +96,26 @@ public class FallbackAPIProcessor extends HttpUploadProcessor {
 	                return;
 	            }
 	            
-				String sourcePlayerID = acc.getAccountID();
-				
-				// log details
-				if (System.getProperty("debugMode") != null) {
-					System.out.println("[API] [r/block] inbound: ( source: " + sourcePlayerID + " )");
-				}
-
-				// Request
-				String id = path.substring("/r/block/".length());
-
-				// Send response
 				JsonObject response = new JsonObject();
-				// TODO: blocking users
-				response.addProperty("error", "not_blocked");
+	            switch(method.toLowerCase())
+	            {
+	            	case "get":
+	            	{
+	    				String sourcePlayerID = acc.getAccountID();
+	    				
+	    				// log details
+	    				if (System.getProperty("debugMode") != null) {
+	    					System.out.println("[API] [r/block] [" + method + "] inbound: ( source: " + sourcePlayerID + " )");
+	    				}
+
+	    				// Request
+	    				String id = path.substring("/r/block/".length());
+
+	    				// Send response
+	    				response.addProperty("error", "not_blocked");     		
+	            	}
+	            }
+	            
 				setBody(response.toString());
 			} else if (path.startsWith("/r/follow/")) {
 				// Parse JWT payload
@@ -149,82 +157,59 @@ public class FallbackAPIProcessor extends HttpUploadProcessor {
 				
 				// log details
 				if (System.getProperty("debugMode") != null) {
-					System.out.println("[API] [r/follow] inbound: ( source: " + sourcePlayerID + ", target: " + targetPlayerID + " )");
+					System.out.println("[API] [r/follow] [" + method + "] inbound: ( source: " + sourcePlayerID + ", target: " + targetPlayerID + " )");
 				}
 				
 				//open friend list
 				FriendListManager.getInstance().openFriendList(sourcePlayerID);
 				FriendListManager.getInstance().openFriendList(targetPlayerID);
 				
-				if(!FriendListManager.getInstance().getPlayerIsFollowing(sourcePlayerID, targetPlayerID))
+				switch(method.toLowerCase())
 				{
-					//doing date stuff ahead of time
-					SimpleDateFormat fmt = new SimpleDateFormat("YYYY-MM-dd'T'HH:mm:ss.'0Z'");
-					fmt.setTimeZone(TimeZone.getTimeZone("UTC"));
-					String createdAt = fmt.format(new Date());
-					
-					//add player to friend list
-					FriendListEntry entry = new FriendListEntry();
-					entry.addedAt = createdAt;
-					entry.playerID = targetPlayerID;
-					entry.favorite = false;
-					
-					FriendListManager.getInstance().addFollowingPlayer(sourcePlayerID, entry);
-					
-					//for the other player, add them to followers
-					FriendListEntry followerEntry = new FriendListEntry();
-					followerEntry.addedAt = createdAt;
-					followerEntry.playerID = sourcePlayerID;
-					followerEntry.favorite = false;
-					
-					FriendListManager.getInstance().addFollowerPlayer(targetPlayerID, followerEntry);
-				}
-				else
-				{
-					//must be trying to remove friend					
-					FriendListManager.getInstance().removeFollowingPlayer(sourcePlayerID, targetPlayerID);
-					FriendListManager.getInstance().removeFollowerPlayer(targetPlayerID, sourcePlayerID);
-				}
-							
-				//construct response packet	
-				JsonObject response = new JsonObject();
-				response.addProperty("error", ""); // no error?
-				setBody(response.toString());		
-				
-				// log interaction details
-				if (System.getProperty("debugMode") != null) {
-					System.out.println("[API] [r/follow] outbound: ( " + response.toString() + " )");
-				}
-				
+					case "post":
+					{
+						//doing date stuff ahead of time
+						SimpleDateFormat fmt = new SimpleDateFormat("YYYY-MM-dd'T'HH:mm:ss.'0Z'");
+						fmt.setTimeZone(TimeZone.getTimeZone("UTC"));
+						String createdAt = fmt.format(new Date());
+						
+						//add player to friend list
+						FriendListEntry entry = new FriendListEntry();
+						entry.addedAt = createdAt;
+						entry.playerID = targetPlayerID;
+						entry.favorite = false;
+						
+						FriendListManager.getInstance().addFollowingPlayer(sourcePlayerID, entry);
+						
+						//for the other player, add them to followers
+						FriendListEntry followerEntry = new FriendListEntry();
+						followerEntry.addedAt = createdAt;
+						followerEntry.playerID = sourcePlayerID;
+						followerEntry.favorite = false;
+						
+						FriendListManager.getInstance().addFollowerPlayer(targetPlayerID, followerEntry);
+
+						setResponseCode(201);
+						
+						// log interaction details
+						if (System.getProperty("debugMode") != null) {
+							System.out.println("[API] [r/follow] Processed friend request, sending 201... ");
+						}
+						
+						break;						
+					}
+					case "delete":
+					{
+						//must be trying to remove friend					
+						FriendListManager.getInstance().removeFollowingPlayer(sourcePlayerID, targetPlayerID);
+						FriendListManager.getInstance().removeFollowerPlayer(targetPlayerID, sourcePlayerID);
+						break;
+					}	
+				}				
 			} else if (path.startsWith("/r/followers")) {
-				// Parse JWT payload
-				String token = this.getHeader("Authorization").substring("Bearer ".length());
-
-				// Verify signature
-				String verifyD = token.split("\\.")[0] + "." + token.split("\\.")[1];
-				String sig = token.split("\\.")[2];
-				if (!EmuFeral.verify(verifyD.getBytes("UTF-8"), Base64.getUrlDecoder().decode(sig))) {
-					this.setResponseCode(401);
-					this.setResponseMessage("Access denied");
-					return;
-				}
 				
-	             // Verify expiry
-	            JsonObject jwtPl = JsonParser
-	                    .parseString(new String(Base64.getUrlDecoder().decode(token.split("\\.")[1]), "UTF-8"))
-	                    .getAsJsonObject();
-	            if (!jwtPl.has("exp") || jwtPl.get("exp").getAsLong() < System.currentTimeMillis() / 1000) {
-	                this.setResponseCode(401);
-	                this.setResponseMessage("Access denied");
-	                return;
-	            }
-
-	            JsonObject payload = JsonParser
-	                    .parseString(new String(Base64.getUrlDecoder().decode(token.split("\\.")[1]), "UTF-8"))
-	                    .getAsJsonObject();
-	            
-	            // Find account
-	            EmuFeralAccount acc = manager.getAccount(payload.get("uuid").getAsString());
+				//verify token and get the account.
+				var acc = verifyAndGetAcc(manager);
 	            if (acc == null) {
 	                this.setResponseCode(401);
 	                this.setResponseMessage("Access denied");
@@ -236,7 +221,7 @@ public class FallbackAPIProcessor extends HttpUploadProcessor {
 				// log details
 				if (System.getProperty("debugMode") != null) {
 					System.out.println(
-							"[API] [r/followers]  Client to server ( source: " + sourcePlayerID + " )");
+							"[API] [r/followers] [" + method + "] Client to server ( source: " + sourcePlayerID + " )");
 				}
 
 				// TODO: UPDATED AT FIELD.
@@ -276,34 +261,10 @@ public class FallbackAPIProcessor extends HttpUploadProcessor {
 				}
 
 			} else if (path.startsWith("/r/followings")) {
-				// Parse JWT payload
-				String token = this.getHeader("Authorization").substring("Bearer ".length());
 
-				// Verify signature
-				String verifyD = token.split("\\.")[0] + "." + token.split("\\.")[1];
-				String sig = token.split("\\.")[2];
-				if (!EmuFeral.verify(verifyD.getBytes("UTF-8"), Base64.getUrlDecoder().decode(sig))) {
-					this.setResponseCode(401);
-					this.setResponseMessage("Access denied");
-					return;
-				}
+				//verify token and get the account.
+				var acc = verifyAndGetAcc(manager);
 				
-	             // Verify expiry
-	            JsonObject jwtPl = JsonParser
-	                    .parseString(new String(Base64.getUrlDecoder().decode(token.split("\\.")[1]), "UTF-8"))
-	                    .getAsJsonObject();
-	            if (!jwtPl.has("exp") || jwtPl.get("exp").getAsLong() < System.currentTimeMillis() / 1000) {
-	                this.setResponseCode(401);
-	                this.setResponseMessage("Access denied");
-	                return;
-	            }
-
-	            JsonObject payload = JsonParser
-	                    .parseString(new String(Base64.getUrlDecoder().decode(token.split("\\.")[1]), "UTF-8"))
-	                    .getAsJsonObject();
-	            
-	            // Find account
-	            EmuFeralAccount acc = manager.getAccount(payload.get("uuid").getAsString());
 	            if (acc == null) {
 	                this.setResponseCode(401);
 	                this.setResponseMessage("Access denied");
@@ -315,7 +276,7 @@ public class FallbackAPIProcessor extends HttpUploadProcessor {
 				// log details
 				if (System.getProperty("debugMode") != null) {
 					System.out.println(
-							"[API] [r/followings]  Client to server ( source: " + sourcePlayerID + " )");
+							"[API] [r/followings] [" + method + "]  Client to server ( source: " + sourcePlayerID + " )");
 				}
 				
 				//open friend list
@@ -351,22 +312,34 @@ public class FallbackAPIProcessor extends HttpUploadProcessor {
 				if (System.getProperty("debugMode") != null) {
 					System.out.println("[API] [r/followings] outbound: ( " + jsonArray.toString() + " )");
 				}
+			} else if (path.startsWith("r/favorite")) {
+				
+				
+				//TODO: Favouriting
+				
+				
+			} else if (path.startsWith("/s/desktop")) {
+
+				//dud handler
+				
+				// log details
+				if (System.getProperty("debugMode") != null) {
+					System.out.println(
+							"[API] [/s/desktop] [" + method + "] DUD");
+				}			
 			} else {
 				// log details
 				if (System.getProperty("debugMode") != null) {
-					System.err.println("[API] Unhandled Api Call: ( path:" + path + " ) ( body: " + body + " )");
+					System.err.println("[API] Unhandled Api Call: ( path:" + path + " ) ( method: " + method + " ) ( body: " + body + " )");
 				}
 
 				setResponseCode(400);
-				path = path;
 				setBody("{}");
 			}
 		} catch (Exception e) {
 			if (System.getProperty("debugMode") != null) {
 				System.err.println("[FALLBACKAPI] ERROR : " + e.getMessage() + " )");
 			}
-			
-			e = e;
 		}
 	}
 
@@ -388,6 +361,36 @@ public class FallbackAPIProcessor extends HttpUploadProcessor {
 	@Override
 	public boolean supportsChildPaths() {
 		return true;
+	}
+	
+	private EmuFeralAccount verifyAndGetAcc(AccountManager manager) throws JsonSyntaxException, UnsupportedEncodingException
+	{
+		// Parse JWT payload
+		String token = this.getHeader("Authorization").substring("Bearer ".length());
+
+		// Verify signature
+		String verifyD = token.split("\\.")[0] + "." + token.split("\\.")[1];
+		String sig = token.split("\\.")[2];
+		if (!EmuFeral.verify(verifyD.getBytes("UTF-8"), Base64.getUrlDecoder().decode(sig))) {
+			return null;
+		}
+		
+         // Verify expiry
+        JsonObject jwtPl = JsonParser
+                .parseString(new String(Base64.getUrlDecoder().decode(token.split("\\.")[1]), "UTF-8"))
+                .getAsJsonObject();
+        if (!jwtPl.has("exp") || jwtPl.get("exp").getAsLong() < System.currentTimeMillis() / 1000) {
+            return null;
+        }
+
+        JsonObject payload = JsonParser
+                .parseString(new String(Base64.getUrlDecoder().decode(token.split("\\.")[1]), "UTF-8"))
+                .getAsJsonObject();
+        
+        // Find account
+        EmuFeralAccount acc = manager.getAccount(payload.get("uuid").getAsString());
+        
+        return acc;
 	}
 
 }
