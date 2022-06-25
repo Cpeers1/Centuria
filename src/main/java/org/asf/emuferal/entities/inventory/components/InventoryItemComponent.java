275bcde9923117cb1af6337a7ee8f6dbb6192e80
Cpeers1 <Owen9456@gmail.com>
2022-06-25 10:25:05 -0600

    Removed unused factory import.
    Signed-off-by: Cpeers1 <Owen9456@gmail.com>

--- src/main/java/org/asf/emuferal/entities/inventory/components/InventoryItemComponent.java
+++ src/main/java/org/asf/emuferal/entities/inventory/components/InventoryItemComponent.java
@@ -4,59 +4,61 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.asf.emuferal.entities.Factory;
-
 import com.google.gson.JsonObject;
 
 /**
  * Base class for all inventory item components.
+ * 
  * @author owen9
  *
  */
 public abstract class InventoryItemComponent {
 
 	/**
-	 * Gets the component name. 
-	 * This should be the name the object is stored as under the components part of the inventory item.
+	 * Gets the component name. This should be the name the object is stored as
+	 * under the components part of the inventory item.
+	 * 
 	 * @return The component's name.
 	 */
 	public abstract String getComponentName();
-	
+
 	/**
 	 * Converts this component to a JsonObject.
+	 * 
 	 * @return The JsonObject component.
 	 */
 	public abstract JsonObject toJson();
-	
-	
+
 	/**
 	 * Populates the component with values from the jsonObject.
 	 */
 	protected abstract void getPropertiesFromJson(JsonObject object);
-	
+
 	/**
 	 * Populates a new component of type T with properties from a JsonObject.
-	 * @param <T> The type of the component.
-	 * @param type The type of the component.
+	 * 
+	 * @param <T>        The type of the component.
+	 * @param type       The type of the component.
 	 * @param jsonObject The object to retrieve properties from.
 	 * @return A new inventory item component of type T.
-	 * @throws SecurityException 
-	 * @throws NoSuchMethodException 
-	 * @throws InvocationTargetException 
-	 * @throws IllegalArgumentException 
-	 * @throws IllegalAccessException 
-	 * @throws InstantiationException 
+	 * @throws SecurityException
+	 * @throws NoSuchMethodException
+	 * @throws InvocationTargetException
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws InstantiationException
 	 */
-	public static <T extends InventoryItemComponent> InventoryItemComponent fromJson(Class<T> type, JsonObject jsonObject) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
-		//create a new component of type t 
+	public static <T extends InventoryItemComponent> InventoryItemComponent fromJson(Class<T> type,
+			JsonObject jsonObject) throws InstantiationException, IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException, NoSuchMethodException, SecurityException {
+		// create a new component of type t
 		var newComponent = type.getDeclaredConstructor().newInstance();
-		
-		//populate it with properties from the json object.
+
+		// populate it with properties from the json object.
 		newComponent.getPropertiesFromJson(jsonObject);
-		
-		//return it
+
+		// return it
 		return newComponent;
 	}
-	
 
 }
