90a4b0579edbceac9f3750a133d6c96354caf981
Joey743 <70813324+Joey743@users.noreply.github.com>
2022-08-19 05:10:34 -0400

    Improve AvatarAction

--- src/main/java/org/asf/centuria/packets/xt/gameserver/players/AvatarAction.java
+++ src/main/java/org/asf/centuria/packets/xt/gameserver/players/AvatarAction.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.asf.centuria.data.XtReader;
 import org.asf.centuria.data.XtWriter;
+import org.asf.centuria.enums.actors.ActorActionType;
 import org.asf.centuria.networking.gameserver.GameServer;
 import org.asf.centuria.networking.smartfox.SmartfoxClient;
 import org.asf.centuria.packets.xt.IXtPacket;
@@ -36,8 +37,12 @@ public class AvatarAction implements IXtPacket<AvatarAction> {
 
 	@Override
 	public boolean handle(SmartfoxClient client) throws IOException {
-		// Avatar action
+
+		//Set action
 		Player plr = (Player) client.container;
+		plr.lastAction = ActorActionType.getAvatarActionType(action);
+
+		//Write packet
 		XtWriter pk = new XtWriter();
 		pk.writeString("ou");
 		pk.writeInt(-1); // Data prefix
@@ -55,57 +60,7 @@ public class AvatarAction implements IXtPacket<AvatarAction> {
 		pk.writeString("0");
 		pk.writeString("0");
 		pk.writeDouble(plr.lastRotW);
-		switch (action) {
-		case "8930": { // Sleep
-			plr.lastAction = 40;
-			break;
-		}
-		case "9108": { // Tired
-			plr.lastAction = 41;
-			break;
-		}
-		case "9116": { // Sit
-			plr.lastAction = 60;
-			break;
-		}
-		case "9121": { // Mad
-			plr.lastAction = 70;
-			break;
-		}
-		case "9122": { // Excite
-			plr.lastAction = 80;
-			break;
-		}
-		case "9143": { // Sad
-			plr.lastAction = 180;
-			break;
-		}
-		case "9151": { // Flex
-			plr.lastAction = 200;
-			break;
-		}
-		case "9190": { // Play
-			plr.lastAction = 210;
-			break;
-		}
-		case "9147": { // Scared
-			plr.lastAction = 190;
-			break;
-		}
-		case "9139": { // Eat
-			plr.lastAction = 170;
-			break;
-		}
-		case "9131": { // Yes
-			plr.lastAction = 110;
-			break;
-		}
-		case "9135": { // No
-			plr.lastAction = 120;
-			break;
-		}
-		}
-		pk.writeInt(plr.lastAction);
+		pk.writeInt(plr.lastAction.value);
 		pk.writeString(""); // Data suffix
 
 		// Broadcast sync
@@ -115,6 +70,7 @@ public class AvatarAction implements IXtPacket<AvatarAction> {
 				player.client.sendPacket(pk.encode());
 			}
 		}
+		
 
 		return true;
 	}
