4aff5684fb13ffbbd4c8200e611ccdd798e38772
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2022-12-01 12:01:18 +0100

    WIP client mod work (i should switch branches, but i depend on beta 1.5 e)

---
+++ clientmods/feraltweaks/Patches/AssemblyCSharp/BaseDefPatch.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using Il2CppSystem.Collections.Generic;
+
+namespace feraltweaks.Patches.AssemblyCSharp
+{
+    public class BaseDefPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(MethodType.Getter)]
+        [HarmonyPatch(typeof(BaseDef), nameof(BaseDef.DefIDToChart))]
+        public static void DefIDToChart(ref object __result)
+        {
+            Dictionary<string, ChartDataObject> res = (Dictionary<string, ChartDataObject>)__result;
+            foreach (string id in CoreChartDataManagerPatch.DefCache.Keys)
+            {
+                if (res.ContainsKey(id))
+                {
+                    res[id].GetDef(id)._components = CoreChartDataManagerPatch.DefCache[id]._components;
+                }
+            }
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(BaseDef), "GetDef")]
+        public static bool GetDef(string inDefID, ref BaseDef __result)
+        {
+            if (CoreChartDataManagerPatch.DefCache.ContainsKey(inDefID))
+            {
+                __result = CoreChartDataManagerPatch.DefCache[inDefID];
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
