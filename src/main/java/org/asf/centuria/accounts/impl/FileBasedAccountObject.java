d46b3e1785fd16a1bcfb0f97cfa27067b5d0565c
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2023-01-27 02:30:08 +0100

    Fix mistake

--- src/main/java/org/asf/centuria/accounts/impl/FileBasedAccountObject.java
+++ src/main/java/org/asf/centuria/accounts/impl/FileBasedAccountObject.java
@@ -207,6 +207,7 @@ public class FileBasedAccountObject extends CenturiaAccount {
 			loginName = username;
 			Files.writeString(new File("accounts/" + userUUID).toPath(),
 					userUUID + "\n" + loginName + "\n" + isNew + "\n" + displayName + "\n" + userID);
+			Files.writeString(new File("accounts/" + loginName).toPath(), userUUID + "\n" + loginName);
 			return true;
 		} catch (IOException e) {
 		}
