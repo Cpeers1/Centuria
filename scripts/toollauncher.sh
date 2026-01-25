609cabbc208cfee09b4d7d8e7e94d1b9a1c8cb65
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2026-01-25 17:45:20 +0100

    Try to make the tools scripts work on msys/cygwin/mingw

--- scripts/toollauncher.sh
+++ scripts/toollauncher.sh
@@ -2,8 +2,18 @@
 chmod +x "$0"
 dirpath="$(dirname "$0")"
 
-libs=$(find "$dirpath/libs/" -name '*.jar' -exec echo -n :{} \;)
+plat="$(uname -s | tr '[:upper:]' '[:lower:]')"
+if [ "${plat//_*/}" == "cygwin" ] || [ "${plat//_*/}" == "mingw64" ]; then
+	plat="${plat//_*/}"
+fi
+sep=":"
+if [ "$plat" == "cygwin" ] || [ "$plat" == "mingw64" ] ; then
+    # Msys support
+    sep=";"
+fi
+
+libs=$(find "$dirpath/libs/" -name '*.jar' -exec echo -n "$sep{}" \;)
 libs=${libs:1}
-	
+
 java -cp "$libs" "$@"
 exit $?
